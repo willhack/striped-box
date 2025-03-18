50468f532b1a03e5dc6e342e45ec7f3172d06d6a
Will Hack <18036720+willhack@users.noreply.github.com>
2025-03-18 16:12:11 -0400

    batman

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+export default function App() {
+  return (
+    <main>
+      <nav>
+        <h1>The striped box is visible</h1>
+      </nav>
+      <section>
+        <div className="purple box" />
+        <div className="rebeccapurple box" />
+        <div className="blueviolet box" />
+        <div className="darkorchid box" />
+        <div className="darkslategray box" />
+        <div className="blue box" />
+        <div className="mediumblue box" />
+        <div className="indigo box" />
+        <div className="midnightblue box" />
+        <div className="striped box" />
+        <div className="navy box" />
+        <div className="slateblue box" />
+      </section>
+    </main>
+  );
+}
